81279496f4adbd2c7c96b0f3688cc638f1182733
Skyler H <skylerh15@github.com>
2022-02-13 03:29:44 -0600

    added proxy to fix cors on local network

--- frontend/src/app/api/base-api.ts
+++ frontend/src/app/api/base-api.ts
@@ -1,7 +1,7 @@
 import { Endpoints } from "enums";
 
 function fetchFromTauri<U>(url: Endpoints, params: string): Promise<U> {
-  return fetch(`http://localhost:8001/${url}/${params}`).then(
+  return fetch(`${url}/${params}`).then(
     response => response.json() as Promise<U>
   );
 }
