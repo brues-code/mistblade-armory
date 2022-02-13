e365219d70bf308651c2dc9505d4a1ad9dca4109
Skyler H <skylerh15@github.com>
2022-02-12 23:49:16 -0600

    CORS fix

--- node/src/server.ts
+++ node/src/server.ts
@@ -4,11 +4,19 @@ const app = express();
 
 import controllers from "../src/controllers/controller";
 
-app.get("/charsheet/:name", (req, res) => controllers.getCharSheet(req, res));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/charsheet/:name", controllers.getCharSheet);
 
 const port = process.env.PORT || 8001;
 
 app.listen(port, () => {
-    console.log(process.env.APIKEY);
-    console.log(`Listening to port http://localhost:${port}`);
+  console.log(`Listening to port http://localhost:${port}`);
 });
