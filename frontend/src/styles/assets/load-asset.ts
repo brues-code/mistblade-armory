b60b735075aa1f364a46cb4d9d0cf55a0931ec21
Skyler H <skylerh15@github.com>
2022-02-16 06:43:09 -0600

    added empty slot icons

--- frontend/src/styles/assets/load-asset.ts
+++ frontend/src/styles/assets/load-asset.ts
@@ -1,9 +1,11 @@
 import { Class, Race, Gender } from "enums";
+import { SlotIcons } from "app-constants";
 
-export const getCharacterIcon = (raceId: Race, genderId: Gender) => {
-    return require(`./race/${raceId}_${genderId}.jpg`);
-};
+export const getCharacterIcon = (raceId: Race, genderId: Gender) =>
+  require(`./race/${raceId}_${genderId}.jpg`);
 
-export const getClassIcon = (classId: Class) => {
-    return require("./class/" + classId + ".png");
-};
+export const getClassIcon = (classId: Class) =>
+  require(`./class/${classId}.png`);
+
+export const getEmptySlotIcon = (slotIndex: number) =>
+  require(`./slot/UI-PaperDoll-Slot-${SlotIcons[slotIndex]}.png`);
