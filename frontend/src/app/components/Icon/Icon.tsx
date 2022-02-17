14f17f00058a4e6469004bc345c861f32a29fa17
Skyler H <skylerh15@github.com>
2022-02-17 04:20:45 -0600

    icon.tsx cleanup

--- frontend/src/app/components/Icon/Icon.tsx
+++ frontend/src/app/components/Icon/Icon.tsx
@@ -1,38 +1,11 @@
-import * as React from 'react';
-import styled from 'styled-components';
+import React, { FC } from 'react';
 import svg4Everybody from 'svg4everybody';
+
+import { StyledIcon, StyledIconContainer } from './styles';
 
 const icons = require('styles/resources/icons.svg').default;
 
 svg4Everybody();
-
-interface IconProps {
-    width?: string;
-    height?: string;
-    fill?: string;
-}
-
-interface IconContainerProps {
-    display?: string;
-    className?: string;
-    onClick?: Function;
-}
-
-const StyledIcon = styled.svg<IconProps>`
-    fill: ${props => props.fill || 'currentColor'};
-    width: ${props => props.width || '18px'};
-    height: ${props => props.height || '18px'};
-`;
-
-const StyledIconContainer = styled.div<IconContainerProps>`
-    width: fit-content;
-    height: fit-content;
-    &.pointer {
-        cursor: pointer;
-    }
-    display: ${props => props.display || 'flex'};
-    ${props => props.onClick && `cursor: pointer;`}
-`;
 
 export interface IconComponentProps {
     className?: string;
@@ -48,23 +21,13 @@ export interface IconComponentProps {
     width?: string;
 }
 
-class Icon extends React.Component<IconComponentProps> {
-    public static defaultProps: Partial<IconComponentProps> = {
-        fill: 'currentColor'
-    };
-
-    render() {
-        const { name, width, height, fill, title, innerRef, ...restProps } = this.props;
-
-        return (
-            <StyledIconContainer ref={innerRef} {...restProps}>
-                <StyledIcon width={width} height={height} fill={fill}>
-                    {title && <title>{title}</title>}
-                    <use xlinkHref={icons + '#' + name} />
-                </StyledIcon>
-            </StyledIconContainer>
-        );
-    }
-}
+const Icon: FC<IconComponentProps> = ({ name, width, height, fill, title, innerRef, ...restProps }) => (
+    <StyledIconContainer ref={innerRef} {...restProps}>
+        <StyledIcon width={width} height={height} fill={fill || 'currentColor'}>
+            {title && <title>{title}</title>}
+            <use xlinkHref={icons + '#' + name} />
+        </StyledIcon>
+    </StyledIconContainer>
+);
 
 export default Icon;
